7c90beaaa36f065b678df108dbc7c3620ed2c94d
gabbes <gabtost@gmail.com>
2019-11-12 21:11:56 +0100

    feat: extend eslint:recommended

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ module.exports = {
   },
 
   extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:import/typescript",
     "plugin:react/recommended",
