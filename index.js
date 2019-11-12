a949d9b11e4a6019a28c2ff827279473df4ae912
gabbes <gabtost@gmail.com>
2019-11-12 21:11:28 +0100

    style: use double quotes

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
 
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2019,
-    sourceType: 'module',
+    sourceType: "module"
   },
 
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript',
-    'plugin:react/recommended',
-    'plugin:react-native-a11y/recommended',
-    'prettier/@typescript-eslint',
-    'prettier/react',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "plugin:react/recommended",
+    "plugin:react-native-a11y/recommended",
+    "prettier/@typescript-eslint",
+    "prettier/react"
   ],
 
-  plugins: ['import', 'react-hooks', 'react-native', 'react-native-a11y'],
+  plugins: ["import", "react-hooks", "react-native", "react-native-a11y"],
 
   env: {
-    'react-native/react-native': true,
+    "react-native/react-native": true
   },
 
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: "detect"
+    }
   },
 
   rules: {
-    '@typescript-eslint/explicit-function-return-type': [
+    "@typescript-eslint/explicit-function-return-type": [
       1,
       {
         allowExpressions: true,
-        allowTypedFunctionExpressions: true,
-      },
+        allowTypedFunctionExpressions: true
+      }
     ],
-    '@typescript-eslint/no-unused-vars': 0,
-    '@typescript-eslint/no-use-before-define': 0,
-    'import/first': 1,
-    'import/newline-after-import': 1,
-    'import/no-cycle': 2,
-    'import/no-default-export': 1,
-    'import/no-duplicates': 1,
-    'react/prop-types': 0,
-    'react-hooks/exhaustive-deps': 1,
-    'react-hooks/rules-of-hooks': 2,
-  },
+    "@typescript-eslint/no-unused-vars": 0,
+    "@typescript-eslint/no-use-before-define": 0,
+    "import/first": 1,
+    "import/newline-after-import": 1,
+    "import/no-cycle": 2,
+    "import/no-default-export": 1,
+    "import/no-duplicates": 1,
+    "react/prop-types": 0,
+    "react-hooks/exhaustive-deps": 1,
+    "react-hooks/rules-of-hooks": 2
+  }
 };
